992665cf1ef00c10b705a8e3003656d80503dd34
Alex Yagodkin <adamsHillzzz@gmail.com>
2026-01-18 19:49:08 +0300

    feat: update CMakeLists and include GLAD library

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
 
 int main(void)
 {
@@ -7,6 +9,11 @@ int main(void)
     /* Initialize the library */
     if (!glfwInit())
         return -1;
+    
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
@@ -18,6 +25,15 @@ int main(void)
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
+    
+    if (!gladLoadGL()) {
+        std::cout << "Can't load GLAD" << std::endl;
+        return -1;
+    }
+    
+    std::cout << "OpenGl " << GLVersion.major << "." << GLVersion.minor << std::endl;
+    
+    glClearColor(0, 1, 0, 1);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
